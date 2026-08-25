fae9de5e79767569afd49bdc78541b018788b24a
Russ White <356303+russwyte@users.noreply.github.com>
2026-08-25 18:34:39 -0500

    Bump sbt-zipx to 0.8.1 and sbt to 2.0.7 (#73)

--- project/ZipxVersions.scala
+++ project/ZipxVersions.scala
@@ -6,7 +6,7 @@ import zipx.*
   * already brings it in. Action pins stay on jar defaults.
   */
 object MyVersions extends ZipxVersions:
-  val sbt: SbtVersion     = SbtVersion("2.0.6")
+  val sbt: SbtVersion     = SbtVersion("2.0.7")
   val scala: ScalaVersion = ScalaVersion("3.8.4")
 
   val zio        = Lib("dev.zio", "zio", "2.1.26")
