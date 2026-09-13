8ac098020a0fee5d766d4523bfb52fbce95e4d18
Russ White <356303+russwyte@users.noreply.github.com>
2026-09-13 18:36:57 -0500

    Bump sbt to 2.1.0-M1
    Metabuild is Scala 3.9.0, matching heddle / chekhov 0.1.0 TASTy. Needed
    before the next ascent-preview (heddle) can drop zio-http here.

--- project/ZipxVersions.scala
+++ project/ZipxVersions.scala
@@ -6,7 +6,7 @@ import zipx.*
   * already brings it in. Action pins stay on jar defaults.
   */
 object MyVersions extends ZipxVersions:
-  val sbt: SbtVersion     = SbtVersion("2.0.7")
+  val sbt: SbtVersion     = SbtVersion("2.1.0-M1")
   val scala: ScalaVersion = ScalaVersion("3.9.0")
 
   val zio        = Lib("dev.zio", "zio", "2.1.26")
